7b454596cf1aab8c70b772c9c223e0003c73cd51
Javascriptyo <80660509+Javascriptyo@users.noreply.github.com>
2022-01-19 11:31:32 -0500

    Add files via upload
    write npm i in termnial

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+
+const data = [
+  { number: 59, text: 'Java Script Projects'},
+  { number: 280, text: 'Html Projects', },
+  { number: 3, text: 'Css Projects', },
+  { number: 7, text: 'Php projects', }
+];
+
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}+`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider/>
+  </Section>
+);
+
+export default Acomplishments;
---
+++ src/constants/constants.js
@@ -0,0 +1,27 @@
+export const projects = [{
+        title: 'Race Car Game',
+        description: "Using React, Node.js, Express & MongoDB I made this Car Game",
+        image: '/images/OIP.jpg',
+        tags: ['Mongo', 'Express', 'React', 'Node'],
+        source: 'https://github.com/Javascriptyo',
+        visit: 'https://github.com/Javascriptyo',
+        id: 0,
+    },
+    {
+        title: 'Portfolio',
+        description: "Using React, Node.js, Express & MongoDB I made this Portfolio",
+        image: '/images/Nak.jpeg',
+        tags: ['React', 'JavaScript'],
+        source: 'https://github.com/Javascriptyo',
+        visit: 'https://github.com/Javascriptyo',
+        id: 1,
+    },
+];
+
+export const TimeLineData = [
+    { year: 2018, text: 'Started my journey', },
+    { year: 2019, text: 'Started Html', },
+    { year: 2020, text: 'Started Css and Mastered Html', },
+    { year: 2021, text: 'Started Javascript and Mastered Css', },
+    { year: 2022, text: 'Became a expert Frontend devloper and mastered Javascript with React and Started Java', },
+];
